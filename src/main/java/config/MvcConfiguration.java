004040ea93221459625ceb940796780b3ee98e85
Chris <ponyboy712@gmail.com>
2016-10-01 21:18:27 -0400

    web.xml, config

--- src/main/java/config/MvcConfiguration.java
+++ src/main/java/config/MvcConfiguration.java
@@ -1,7 +1,23 @@
 package config;
 
-/**
- * Created by ponyboy on 10/1/16.
- */
-public class MvcConfiguration {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@ComponentScan({"config", "controller"})
+@EnableWebMvc
+public class MvcConfiguration extends WebMvcConfigurerAdapter {
+
+    @Bean
+    public ViewResolver getViewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("WEB-INF/jsp/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
 }
