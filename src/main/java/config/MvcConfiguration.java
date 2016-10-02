e602bd482690955985801ffb8f89ad418a2d71fb
Chris <ponyboy712@gmail.com>
2016-10-02 14:13:39 -0400

    moving html view to templates sub directory

--- src/main/java/config/MvcConfiguration.java
+++ src/main/java/config/MvcConfiguration.java
@@ -14,7 +14,7 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 public class MvcConfiguration extends WebMvcConfigurerAdapter {
 
     //// FIXME: 10/1/16 this aint working
-    //// TODO: 10/1/16 check this shit out 
+    //// TODO: 10/1/16 check this shit out
     @Bean
     public ViewResolver getViewResolver() {
         InternalResourceViewResolver resolver = new InternalResourceViewResolver();
